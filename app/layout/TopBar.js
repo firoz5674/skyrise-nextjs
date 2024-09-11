4d55e4bb76565da9ebbdc34822121fe02ab0665e
firoz5674 <firozsaifi1234@gmail.com>
2024-09-11 13:04:42 +0530

    fix

--- app/layout/TopBar.js
+++ app/layout/TopBar.js
@@ -5,12 +5,12 @@ import CloseIcon from '../components/svg-icons/close-icon'
 
 const TopBar = () => {
   return (
-    <section className='bg-topbar-gradient pt-3 pb-5 px-4'>
+    <section className='bg-topbar-gradient pt-4 pb-6 px-4'>
       <div className='text-center flex justify-between items-center'>
         <div></div>
         <div className='block lg:flex items-center'>
-          <p className='text-base mr-5 mb-4 lg:mb-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <Link href="" className='btn btn-secondary px-5 pt-[9px] pb-[10px] text-[14px]'>Contact Now</Link>
+          <p className='text-[18px] font-semibold text-black mr-5 mb-4 lg:mb-0'>A home is not just a place, it’s a feeling of comfort and belonging.</p>
+          {/* <Link href="" className='btn btn-secondary px-5 pt-[9px] pb-[10px] text-[14px]'>Contact Now</Link> */}
         </div>
         <div className='cursor-pointer'>
           <CloseIcon color="#000" size={6}  />
